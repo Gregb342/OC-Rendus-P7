e8754f0034e6bfa7ba0013a7c12a8c6aa9d98149
Gregoire Bouteillier <gregoire.bouteillier@gmail.com>
2025-01-22 10:59:41 +0100

    Entity Framework Code First initial create based on POCO list

--- P7CreateRestApi/Data/LocalDbContext.cs
+++ P7CreateRestApi/Data/LocalDbContext.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Dot.Net.WebApi.Domain;
+using Dot.Net.WebApi.Controllers.Domain;
+using Dot.Net.WebApi.Controllers;
 
 namespace Dot.Net.WebApi.Data
 {
@@ -13,5 +15,11 @@ namespace Dot.Net.WebApi.Data
         }
 
         public DbSet<User> Users { get; set;}
+        public DbSet<BidList> Bids { get; set;}
+        public DbSet<CurvePoint> CurvePoints { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<RuleName> Rules { get; set; }
+        public DbSet<Trade> Trades { get; set; }
+
     }
 }
