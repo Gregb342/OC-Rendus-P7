52b3ac1b2dfa5f77cc8791ca8b3328a1712756e1
Gregoire Bouteillier <gregoire.bouteillier@gmail.com>
2025-01-22 12:17:44 +0100

    Repository & Interface OK

---
+++ P7CreateRestApi/Repositories/BidListRepository.cs
@@ -0,0 +1,49 @@
+﻿using Dot.Net.WebApi.Data;
+using Dot.Net.WebApi.Domain;
+using Microsoft.EntityFrameworkCore;
+using P7CreateRestApi.Repositories.Interfaces;
+
+namespace P7CreateRestApi.Repositories
+{
+    public class BidListRepository : IBidListRepository
+    {
+        public readonly LocalDbContext _context;
+
+        public BidListRepository(LocalDbContext context)
+        {
+            _context = context;
+        }   
+
+        public async void AddBidList(BidList bidList)
+        {
+            _context.Bids.Add(bidList);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<BidList>> FindAllBidList()
+        {
+            return await _context.Bids.ToListAsync();
+        }
+
+        public async Task<BidList> FindBidListByIdAsync(int bidId)
+        {
+            return await _context.Bids.FirstOrDefaultAsync(b => b.BidListId == bidId);
+        }
+
+        public async void UpdateBidList(BidList bidList)
+        {
+            _context.Bids.Update(bidList);
+            await _context.SaveChangesAsync();
+        }
+
+        public async void RemoveBidList(int bidId)
+        {
+            BidList bidList = await _context.Bids.FindAsync(bidId);
+            if (bidList is not null)
+            {
+                _context.Bids.Remove(bidList);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ P7CreateRestApi/Repositories/Interfaces/IBidListRepository.cs
@@ -0,0 +1,14 @@
+﻿using Dot.Net.WebApi.Domain;
+
+namespace P7CreateRestApi.Repositories.Interfaces
+{
+    public interface IBidListRepository
+    {
+        void AddBidList(BidList bidList);
+        Task<List<BidList>> FindAllBidList();
+        Task<BidList> FindBidListByIdAsync(int bidId);
+        void UpdateBidList(BidList bidList);
+        void RemoveBidList(int bidId);
+
+    }
+}
