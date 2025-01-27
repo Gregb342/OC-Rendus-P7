1ac158cab00082f34745d9ee9094395fa610f15e
Gregoire Bouteillier <gregoire.bouteillier@wedafr.onmicrosoft.com>
2025-01-27 16:32:09 +0100

    Ajout Logique Seed pour l'objet Bid, à completer

--- P7CreateRestApi/Data/LocalDbContext.cs
+++ P7CreateRestApi/Data/LocalDbContext.cs
@@ -2,24 +2,29 @@ using Microsoft.EntityFrameworkCore;
 using Dot.Net.WebApi.Domain;
 using Dot.Net.WebApi.Controllers.Domain;
 using Dot.Net.WebApi.Controllers;
+using P7CreateRestApi.Data.Seed;
 
 namespace Dot.Net.WebApi.Data
 {
     public class LocalDbContext : DbContext
     {
-        public LocalDbContext(DbContextOptions<LocalDbContext> options) : base(options) { }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-        }
-
         public DbSet<User> Users { get; set;}
         public DbSet<Bid> Bids { get; set;}
         public DbSet<CurvePoint> CurvePoints { get; set; }
         public DbSet<Rating> Ratings { get; set; }
         public DbSet<RuleName> Rules { get; set; }
         public DbSet<Trade> Trades { get; set; }
+
+        public LocalDbContext(DbContextOptions<LocalDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            #region Seed Data
+            BidSeed.Seed(builder);
+            #endregion
+        }
 
     }
 }
---
+++ P7CreateRestApi/Data/Seed/BidSeed.cs
@@ -0,0 +1,62 @@
+﻿using Dot.Net.WebApi.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace P7CreateRestApi.Data.Seed
+{
+    public class BidSeed
+    {
+        public static void Seed(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Bid>().HasData(
+                new Bid
+                {
+                    Account = "Account1",
+                    BidType = "Type1",
+                    BidQuantity = 100.5,
+                    AskQuantity = 200.0,
+                    BidPrice = 1500.75,
+                    AskPrice = 1520.25,
+                    Benchmark = "Benchmark1",
+                    BidDate = DateTime.UtcNow.AddDays(-5),
+                    Commentary = "Test Commentary 1",
+                    BidSecurity = "Security1",
+                    BidStatus = "Pending",
+                    Trader = "Trader1",
+                    Book = "Book1",
+                    CreationName = "System",
+                    CreationDate = DateTime.UtcNow,
+                    RevisionName = "System",
+                    RevisionDate = DateTime.UtcNow,
+                    DealName = "Deal1",
+                    DealType = "TypeA",
+                    SourceListId = "Source1",
+                    Side = "Buy"
+                },
+                new Bid
+                {
+                    Account = "Account2",
+                    BidType = "Type2",
+                    BidQuantity = 300.0,
+                    AskQuantity = 350.0,
+                    BidPrice = 1600.00,
+                    AskPrice = 1650.50,
+                    Benchmark = "Benchmark2",
+                    BidDate = DateTime.UtcNow.AddDays(-3),
+                    Commentary = "Test Commentary 2",
+                    BidSecurity = "Security2",
+                    BidStatus = "Completed",
+                    Trader = "Trader2",
+                    Book = "Book2",
+                    CreationName = "Admin",
+                    CreationDate = DateTime.UtcNow,
+                    RevisionName = "Admin",
+                    RevisionDate = DateTime.UtcNow,
+                    DealName = "Deal2",
+                    DealType = "TypeB",
+                    SourceListId = "Source2",
+                    Side = "Sell"
+                }
+                );
+        }
+    }
+}
