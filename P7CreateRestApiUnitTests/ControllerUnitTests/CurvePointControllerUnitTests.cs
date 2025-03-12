beb505723f862e5ebef454cb4d1e54b07bb50a4b
Gregoire Bouteillier <gregoire.bouteillier@gmail.com>
2025-03-12 09:15:06 +0100

    Ajout de tests unitaires pour les controllers

--- P7CreateRestApiUnitTests/ControllerUnitTests/CurvePointControllerUnitTests.cs
+++ P7CreateRestApiUnitTests/ControllerUnitTests/CurvePointControllerUnitTests.cs
@@ -1,105 +1,116 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using Moq;
-//using P7CreateRestApi.Services.Interfaces;
-//using P7CreateRestApi.ViewsModels.CurvePoints;
-//using Dot.Net.WebApi.Domain;
-//using Dot.Net.WebApi.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using P7CreateRestApi.Services.Interfaces;
+using P7CreateRestApi.ViewsModels.CurvePoints;
+using Dot.Net.WebApi.Domain;
+using Dot.Net.WebApi.Controllers;
 
-//namespace P7CreateRestApiUnitTests.ControllerUnitTests
-//{
-//    public class CurvePointControllerUnitTests
-//    {
-//        private readonly Mock<ICurvePointService> _mockService;
-//        private readonly CurvePointController _controller;
+namespace P7CreateRestApiUnitTests.ControllerUnitTests
+{
+    public class CurvePointControllerUnitTests
+    {
+        private readonly Mock<ICurvePointService> _mockService;
+        private readonly CurvePointController _controller;
 
-//        public CurvePointControllerTests()
-//        {
-//            _mockService = new Mock<ICurvePointService>();
-//            _controller = new CurvePointController(_mockService.Object);
-//        }
+        public CurvePointControllerUnitTests()
+        {
+            _mockService = new Mock<ICurvePointService>();
+            _controller = new CurvePointController(_mockService.Object);
+        }
 
-//        [Fact]
-//        public async Task AddCurvePoint_ValidModel_ReturnsOk()
-//        {
-//            var model = new AddCurvePointViewModel { CurveId = 1, AsOfDate = DateTime.UtcNow, Term = 5, CurvePointValue = 100 };
+        [Fact]
+        public async Task AddCurvePoint_ValidModel_ReturnsOk()
+        {
+            var model = new AddCurvePointViewModel { CurveId = 1, AsOfDate = DateTime.UtcNow, Term = 5, CurvePointValue = 100 };
 
-//            var result = await _controller.AddCurvePoint(model);
+            var result = await _controller.AddCurvePoint(model);
 
-//            var okResult = Assert.IsType<OkObjectResult>(result);
-//            var returnedCurvePoint = Assert.IsType<CurvePoint>(okResult.Value);
-//            Assert.Equal(model.CurveId, returnedCurvePoint.CurveId);
-//        }
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedCurvePoint = Assert.IsType<CurvePoint>(okResult.Value);
+            Assert.Equal(model.CurveId, returnedCurvePoint.CurveId);
+        }
 
-//        [Fact]
-//        public async Task AddCurvePoint_InvalidModel_ReturnsBadRequest()
-//        {
-//            _controller.ModelState.AddModelError("CurveId", "Required");
-//            var model = new AddCurvePointViewModel();
+        [Fact]
+        public async Task AddCurvePoint_InvalidModel_ReturnsBadRequest()
+        {
+            _controller.ModelState.AddModelError("CurveId", "Required");
+            var model = new AddCurvePointViewModel();
 
-//            var result = await _controller.AddCurvePoint(model);
+            var result = await _controller.AddCurvePoint(model);
 
-//            Assert.IsType<BadRequestObjectResult>(result);
-//        }
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
 
-//        [Fact]
-//        public async Task GetCurvePoint_ExistingId_ReturnsOk()
-//        {
-//            var cp = new CurvePoint { CurveId = 1, AsOfDate = DateTime.UtcNow, Term = 5, CurvePointValue = 100 };
-//            _mockService.Setup(s => s.GetCurvePointByIdAsync(1)).ReturnsAsync(cp);
+        [Fact]
+        public async Task GetCurvePoint_ExistingId_ReturnsOk()
+        {
+            // Arrange
+            var cp = new GetCurvePointViewModel
+            {
+                CurveId = 1,
+                AsOfDate = DateTime.UtcNow,
+                Term = 5,
+                CurvePointValue = 100
+            };
+            _mockService.Setup(s => s.GetCurvePointByIdAsync(1)).ReturnsAsync(cp);
 
-//            var result = await _controller.GetCurvePoint(1);
+            // Act
+            var result = await _controller.GetCurvePoint(1);
 
-//            var okResult = Assert.IsType<OkObjectResult>(result);
-//            var returnedCurvePoint = Assert.IsType<CurvePoint>(okResult.Value);
-//            Assert.Equal(1, returnedCurvePoint.CurveId);
-//        }
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result); 
+            var returnedCurvePoint = Assert.IsType<GetCurvePointViewModel>(okResult.Value); 
+            Assert.Equal(cp.Id, returnedCurvePoint.Id);
+            Assert.Equal(cp.CurveId, returnedCurvePoint.CurveId);
+        }
 
-//        [Fact]
-//        public async Task GetAllCurvePoints_ReturnsOk()
-//        {
-//            var list = new List<CurvePoint> { new CurvePoint { CurveId = 1 }, new CurvePoint { CurveId = 2 } };
-//            _mockService.Setup(s => s.GetAllCurvePoints()).ReturnsAsync(list);
 
-//            var result = await _controller.GetAllCurvePoints();
+        [Fact]
+        public async Task GetAllCurvePoints_ReturnsOk()
+        {
+            var list = new List<GetCurvePointViewModel> { new GetCurvePointViewModel { CurveId = 1 }, new GetCurvePointViewModel { CurveId = 2 } };
+            _mockService.Setup(s => s.GetAllCurvePoints()).ReturnsAsync(list);
 
-//            var okResult = Assert.IsType<OkObjectResult>(result);
-//            var returnedList = Assert.IsType<List<CurvePoint>>(okResult.Value);
-//            Assert.Equal(2, returnedList.Count);
-//        }
+            var result = await _controller.GetAllCurvePoints();
 
-//        [Fact]
-//        public async Task UpdateCurvePoint_ValidModel_ReturnsOk()
-//        {
-//            var model = new UpdateCurvePointViewModel { Id = 1, CurveId = 2, Term = 10, CurvePointValue = 200 };
-//            var updatedCp = new CurvePoint { CurveId = 2, Term = 10, CurvePointValue = 200 };
-//            _mockService.Setup(s => s.UpdateCurvePoint(model)).ReturnsAsync(updatedCp);
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedList = Assert.IsType<List<GetCurvePointViewModel>>(okResult.Value);
+            Assert.Equal(2, returnedList.Count);
+        }
 
-//            var result = await _controller.UpdateCurvePoint(1, model);
+        [Fact]
+        public async Task UpdateCurvePoint_ValidModel_ReturnsOk()
+        {
+            var model = new UpdateCurvePointViewModel { Id = 1, CurveId = 2, Term = 10, CurvePointValue = 200 };
+            var updatedCp = new GetCurvePointViewModel { CurveId = 2, Term = 10, CurvePointValue = 200 };
+            _mockService.Setup(s => s.UpdateCurvePoint(model)).ReturnsAsync(updatedCp);
 
-//            var okResult = Assert.IsType<OkObjectResult>(result);
-//            var returnedCp = Assert.IsType<CurvePoint>(okResult.Value);
-//            Assert.Equal(2, returnedCp.CurveId);
-//        }
+            var result = await _controller.UpdateCurvePoint(1, model);
 
-//        [Fact]
-//        public async Task UpdateCurvePoint_MismatchedId_ReturnsBadRequest()
-//        {
-//            var model = new UpdateCurvePointViewModel { Id = 2 };
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedCp = Assert.IsType<GetCurvePointViewModel>(okResult.Value);
+            Assert.Equal(updatedCp.Id, returnedCp.Id);
+        }
 
-//            var result = await _controller.UpdateCurvePoint(1, model);
+        [Fact]
+        public async Task UpdateCurvePoint_MismatchedId_ReturnsBadRequest()
+        {
+            var model = new UpdateCurvePointViewModel { Id = 2 };
 
-//            Assert.IsType<BadRequestObjectResult>(result);
-//        }
+            var result = await _controller.UpdateCurvePoint(1, model);
 
-//        [Fact]
-//        public async Task DeleteCurvePoint_ValidId_ReturnsOk()
-//        {
-//            _mockService.Setup(s => s.RemoveCurvePoint(1)).Returns(Task.CompletedTask);
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
 
-//            var result = await _controller.DeleteCurvePoint(1);
+        [Fact]
+        public async Task DeleteCurvePoint_ValidId_ReturnsOk()
+        {
+            _mockService.Setup(s => s.RemoveCurvePoint(1)).Returns(Task.CompletedTask);
 
-//            Assert.IsType<OkResult>(result);
-//        }
-//    }
-//}
+            var result = await _controller.DeleteCurvePoint(1);
+
+            Assert.IsType<OkResult>(result);
+        }
+    }
+}
 
---
+++ P7CreateRestApiUnitTests/ControllerUnitTests/RatingControllerUnitTests.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using P7CreateRestApi.Services.Interfaces;
+using P7CreateRestApi.ViewsModels.Ratings;
+using Dot.Net.WebApi.Controllers;
+using Dot.Net.WebApi.Controllers.Domain;
+
+namespace P7CreateRestApiUnitTests.ControllerUnitTests
+{
+    public class RatingControllerUnitTests
+    {
+        private readonly Mock<IRatingService> _mockService;
+        private readonly RatingController _controller;
+
+        public RatingControllerUnitTests()
+        {
+            _mockService = new Mock<IRatingService>();
+            _controller = new RatingController(_mockService.Object);
+        }
+
+        [Fact]
+        public async Task AddRating_ValidModel_ReturnsOk()
+        {
+            // ARRANGE
+            var model = new AddRatingViewModel { MoodysRating = "A", SandPRating = "A", FitchRating = "A", OrderNumber = 1 };
+            var rating = new Rating { MoodysRating = "A", SandPRating = "A", FitchRating = "A", OrderNumber = 1 };
+
+            _mockService.Setup(s => s.AddRating(It.IsAny<Rating>())).Returns(Task.CompletedTask);
+
+            // ACT
+            var result = await _controller.AddRating(model);
+
+            // ASSERT
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedRating = Assert.IsType<Rating>(okResult.Value);
+            Assert.Equal(model.OrderNumber, returnedRating.OrderNumber);
+        }
+
+        [Fact]
+        public async Task AddRating_InvalidModel_ReturnsBadRequest()
+        {
+            // ARRANGE
+            _controller.ModelState.AddModelError("MoodysRating", "Required");
+            var model = new AddRatingViewModel();
+
+            // ACT
+            var result = await _controller.AddRating(model);
+
+            // ASSERT
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task GetRating_ExistingId_ReturnsOk()
+        {
+            // ARRANGE
+            var rating = new GetRatingViewModel { Id = 1, MoodysRating = "A" };
+            _mockService.Setup(s => s.GetRatingByIdAsync(1)).ReturnsAsync(rating);
+
+            // ACT
+            var result = await _controller.GetRating(1);
+
+            // ASSERT
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedRating = Assert.IsType<GetRatingViewModel>(okResult.Value);
+            Assert.Equal(rating.Id, returnedRating.Id);
+        }
+
+        [Fact]
+        public async Task GetAllRatings_ReturnsOk()
+        {
+            // ARRANGE
+            var list = new List<GetRatingViewModel> { new GetRatingViewModel { Id = 1 }, new GetRatingViewModel { Id = 2 } };
+            _mockService.Setup(s => s.GetAllRatings()).ReturnsAsync(list);
+
+            // ACT
+            var result = await _controller.GetAllRatings();
+
+            // ASSERT
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedList = Assert.IsType<List<GetRatingViewModel>>(okResult.Value);
+            Assert.Equal(2, returnedList.Count);
+        }
+
+        [Fact]
+        public async Task UpdateRating_ValidModel_ReturnsOk()
+        {
+            // ARRANGE
+            var model = new UpdateRatingViewModel { Id = 1, MoodysRating = "AA" };
+            var updatedRating = new GetRatingViewModel { Id = 1, MoodysRating = "AA" };
+            _mockService.Setup(s => s.UpdateRating(model)).ReturnsAsync(updatedRating);
+
+            // ACT
+            var result = await _controller.UpdateRating(1, model);
+
+            // ASSERT
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedRating = Assert.IsType<GetRatingViewModel>(okResult.Value);
+            Assert.Equal(updatedRating.Id, returnedRating.Id);
+        }
+
+        [Fact]
+        public async Task UpdateRating_MismatchedId_ReturnsBadRequest()
+        {
+            // ARRANGE
+            var model = new UpdateRatingViewModel { Id = 2 };
+
+            // ACT
+            var result = await _controller.UpdateRating(1, model);
+
+            // ASSERT
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteRating_ValidId_ReturnsOk()
+        {
+            // ARRANGE
+            _mockService.Setup(s => s.RemoveRating(1)).Returns(Task.CompletedTask);
+
+            // ACT
+            var result = await _controller.DeleteRating(1);
+
+            // ASSERT
+            Assert.IsType<OkResult>(result);
+        }
+    }
+}
